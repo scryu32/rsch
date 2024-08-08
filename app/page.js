a8c90bf2f475a283fe347d591b62c1e681414add
rsch <rsch0919@gmail.com>
2024-08-09 00:33:34 +0900

    background

--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ export default function(){
   return (
     <div className="main-page">
       <div>
-        <div className="back-ground">
+        <div className="back-ground-box">
           <h4>Welcome to Rsch Site!</h4>
+          <p>Made by RSC</p>
         </div>
-        <div className="doc-box">
-        </div>
+      </div>
+      <div className="doc-box">
+        
       </div>
     </div>
   )
